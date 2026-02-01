735f8bd8bb49f8235c695aa517fd2084767537cd
Márcio DEV <playdznvlt@gmail.com>
2026-02-01 20:15:46 -0300

    feat: add initial project files

---
+++ src/components/Entity.ts
@@ -0,0 +1,149 @@
+import { Objects } from '@zyrohub/utilities';
+
+export interface EntityToObjectOptions {
+	view?: string;
+
+	[key: string]: any;
+}
+
+export interface EntityOptions<TEntityData, TEntityRelations = {}, TEntityId = string> {
+	id?: TEntityId;
+	idGenerator?: () => TEntityId;
+	data: TEntityData;
+	relations?: TEntityRelations;
+	createdAt?: Date;
+	updatedAt?: Date;
+}
+
+export abstract class Entity<
+	TEntityData extends object,
+	TEntityRelations extends object = {},
+	TEntityToObjectOptions extends EntityToObjectOptions = EntityToObjectOptions,
+	TEntityId = string
+> {
+	protected _id: TEntityId | undefined;
+	protected _createdAt: Date;
+	protected _updatedAt: Date;
+
+	protected readonly _rawData: TEntityData;
+	public readonly data: TEntityData;
+
+	public readonly relations: TEntityRelations;
+
+	private proxyCache = new WeakMap<object, any>();
+	private _changes: Partial<TEntityData> = {};
+
+	constructor(protected options: EntityOptions<TEntityData, TEntityRelations, TEntityId>) {
+		this._id = options.id ?? (options.idGenerator ? options.idGenerator() : (undefined as unknown as TEntityId));
+		this._createdAt = options.createdAt || new Date();
+		this._updatedAt = options.updatedAt || new Date();
+
+		this._rawData = options.data;
+		this.data = this.createProxy(this._rawData);
+
+		this.relations = options.relations || ({} as TEntityRelations);
+	}
+
+	get id() {
+		return this._id;
+	}
+
+	public setId(id: TEntityId): void {
+		this._id = id;
+	}
+
+	private createProxy<T extends object>(target: T, rootKey?: keyof TEntityData): T {
+		return new Proxy(target, {
+			set: (obj, prop, value) => {
+				const key = prop as keyof T;
+				const previousValue = Reflect.get(obj, key);
+
+				if (!Objects.isEqual(previousValue, value)) {
+					const result = Reflect.set(obj, prop, value);
+					const dirtyKey = (rootKey || prop) as keyof TEntityData;
+
+					this.markAsDirty(dirtyKey);
+
+					return result;
+				}
+
+				return Reflect.set(obj, prop, value);
+			},
+			get: (obj, prop) => {
+				const value = Reflect.get(obj, prop);
+
+				if (value && typeof value === 'object') {
+					if (value instanceof Date) return value;
+
+					if (this.proxyCache.has(value)) {
+						return this.proxyCache.get(value);
+					}
+
+					const currentRootKey = (rootKey || prop) as keyof TEntityData;
+					const nestedProxy = this.createProxy(value, currentRootKey);
+
+					this.proxyCache.set(value, nestedProxy);
+					return nestedProxy;
+				}
+
+				return value;
+			}
+		});
+	}
+
+	public toObject(
+		options: TEntityToObjectOptions = {} as TEntityToObjectOptions
+	): TEntityData & TEntityRelations & { id: TEntityId } {
+		const relationsJSON: Record<string, any> = {};
+
+		for (const [key, value] of Object.entries(this.relations)) {
+			if (value === undefined || value === null) {
+				relationsJSON[key] = value;
+				continue;
+			}
+
+			if (typeof value === 'object' && 'toObject' in value && typeof (value as any).toObject === 'function') {
+				relationsJSON[key] = (value as any).toObject(options);
+			} else if (Array.isArray(value)) {
+				relationsJSON[key] = value.map((v: any) =>
+					v && typeof v.toObject === 'function' ? v.toObject(options) : v
+				);
+			} else {
+				relationsJSON[key] = value;
+			}
+		}
+
+		const plainData = Objects.clone(this._rawData);
+
+		const builtObject = {
+			id: this._id,
+			...plainData,
+			relations: relationsJSON,
+			createdAt: this._createdAt,
+			updatedAt: this._updatedAt
+		};
+
+		return this.filterObject(builtObject, options);
+	}
+
+	protected filterObject(data: any, options: TEntityToObjectOptions): any {
+		return data;
+	}
+
+	public toJSON(): any {
+		return this.toObject({ view: 'default' } as TEntityToObjectOptions);
+	}
+
+	private markAsDirty(key: keyof TEntityData) {
+		this._changes[key] = this._rawData[key];
+		this._updatedAt = new Date();
+	}
+
+	public getChanges() {
+		return this._changes;
+	}
+
+	public commit() {
+		this._changes = {};
+	}
+}
---
+++ src/components/EntityGroup.ts
@@ -0,0 +1,43 @@
+import { Entity } from './Entity.js';
+
+export class EntityGroup<T extends Entity<any>> extends Array<T> {
+	constructor(items?: number | T[]) {
+		if (typeof items === 'number') {
+			super(items);
+		} else {
+			super(...(items || []));
+		}
+
+		Object.setPrototypeOf(this, EntityGroup.prototype);
+	}
+
+	add(...items: T[]): this {
+		this.push(...items);
+
+		return this;
+	}
+
+	removeById(id: unknown): boolean {
+		const index = this.findIndex(item => String(item.id) === String(id));
+
+		if (index !== -1) {
+			this.splice(index, 1);
+
+			return true;
+		}
+
+		return false;
+	}
+
+	findById(id: unknown): T | undefined {
+		return this.find(item => String(item.id) === String(id));
+	}
+
+	toObject(options?: Parameters<T['toObject']>[0]): ReturnType<T['toObject']>[] {
+		return this.map(item => item.toObject(options as any));
+	}
+
+	toJSON(): any {
+		return this.toObject({ view: 'default' } as any);
+	}
+}
