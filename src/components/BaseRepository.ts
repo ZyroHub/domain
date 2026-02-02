4c9d3aaf2bbf56893c2d394b33d8eaea10982dc5
Márcio DEV <playdznvlt@gmail.com>
2026-02-01 22:02:56 -0300

    feat: update Repository interface and BaseRepository methods to return entity instances

--- src/components/BaseRepository.ts
+++ src/components/BaseRepository.ts
@@ -1,23 +1,28 @@
+import { EntityId } from '@/types/entity.js';
 import { Repository } from '@/types/repository.js';
 
 import { Entity } from './Entity.js';
 
 export abstract class BaseRepository<T extends Entity<any, any, any>> implements Repository<T> {
-	abstract create(entity: T): Promise<void>;
-	abstract update(entity: T): Promise<void>;
-	abstract delete(id: any): Promise<void>;
+	abstract create(entity: T): Promise<T | void>;
+	abstract update(entity: T): Promise<T | void>;
+	abstract delete(id: EntityId): Promise<void>;
 
-	async save(entity: T): Promise<void> {
+	async save(entity: T): Promise<T | void> {
 		if (entity.exists) {
-			if (!entity.isDirty()) return;
-			await this.update(entity);
+			if (!entity.isDirty()) return entity;
+			const updatedEntity = await this.update(entity);
 
 			entity.commit();
+
+			return updatedEntity || entity;
 		} else {
-			await this.create(entity);
+			const createdEntity = await this.create(entity);
 
 			entity.exists = true;
 			entity.commit();
+
+			return createdEntity || entity;
 		}
 	}
 }
